c74ceef91f504d96d47be37effb94d4cb1e963b6
Luis Fernández <dev.oreku@gmail.com>
2023-05-24 13:17:06 -0600

    feat: create the web service to list the applications

--- src/types/environment.d.ts
+++ src/types/environment.d.ts
@@ -3,6 +3,7 @@ export {}
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
+      API_URL: string
       APPLICATION_URI: string
       OAUTH_CLIENT_ID: string
       OAUTH_CLIENT_SECRET: string
