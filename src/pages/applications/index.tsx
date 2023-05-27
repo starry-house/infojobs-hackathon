2ffdcce64b03d019cae6971d8a43a41ed047ad85
Jose Jose Rios <37422662+joseriosjose@users.noreply.github.com>
2023-05-26 23:09:50 -0600

    feat(applications): if error or no data use mocks instead (#9)

--- src/pages/applications/index.tsx
+++ src/pages/applications/index.tsx
@@ -3,14 +3,26 @@ import { Container, Text, Flex, Stack } from '@chakra-ui/react'
 import { useQuery } from '@tanstack/react-query'
 
 import { IApplication } from '@/types/application'
+import { applications } from '@/mocks/applications.mock'
+import { useMemo } from 'react'
 
 const Applications = () => {
-  const { data } = useQuery<IApplication[]>({
+  const { data, error } = useQuery<IApplication[]>({
     queryKey: ['todos'],
     initialData: [],
     queryFn: () =>
-      fetch('/api/applications').then((response) => response.json()),
+      fetch('/api/applications').then((response) => {
+        if (!response.ok) {
+          throw new Error('Error al obtener los datos')
+        }
+        return response.json()
+      }),
   })
+
+  const applicationData = useMemo(
+    () => (error || data.length == 0 ? applications : data),
+    [error, data],
+  )
 
   return (
     <Flex as="main" alignItems="center">
@@ -25,7 +37,7 @@ const Applications = () => {
           >
             Applications
           </Text>
-          <ApplicationList applicationList={data} />
+          <ApplicationList applicationList={applicationData} />
         </Stack>
       </Container>
     </Flex>
