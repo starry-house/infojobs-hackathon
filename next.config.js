c05a063ad5490d8e09dbe732f4dca83140c576e4
Luis Fernández <54286456+Oreku2608@users.noreply.github.com>
2023-05-28 10:50:04 -0600

    feat: improvements in the user navigation experience. (#15)

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['res.cloudinary.com'],
+  },
+  redirects: async () => [
+    {
+      source: '/app',
+      destination: '/app/applications',
+      permanent: true,
+    },
+  ],
 }
 
 module.exports = nextConfig
