12df01f157ccc9e2bb5ba9f13f02dc3ebc7de9b3
Maykell Carrillo <61121623+macs15@users.noreply.github.com>
2023-05-28 20:01:14 -0300

    Update rules to get application by id either from API or from local data (#16)

---
+++ src/mocks/steps.mock.ts
@@ -0,0 +1,108 @@
+export const steps = [
+  [
+    {
+      current: true,
+      title: 'Submit online application',
+      subtitle: '25/02/23',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit quibusdam consectetur numquam saepe voluptas quaerat, deleniti quidem aperiam ad quos sint nesciunt molestias nemo iusto necessitatibus, repellat aut dignissimos. Eum',
+    },
+    {
+      current: false,
+      title: 'Phone screening interview',
+      subtitle: '10/02/23',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit quibusdam consectetur numquam saepe voluptas quaerat, deleniti quidem aperiam ad quos sint nesciunt molestias nemo iusto necessitatibus, repellat aut dignissimos. Eum',
+    },
+    {
+      current: false,
+      title: 'Interview with hiring manager',
+      subtitle: '20/02/23',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit quibusdam consectetur numquam saepe voluptas quaerat, deleniti quidem aperiam ad quos sint nesciunt molestias nemo iusto necessitatibus, repellat aut dignissimos. Eum',
+    },
+    {
+      current: false,
+      title: 'Interview with technical lead',
+      subtitle: '25/02/23',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit quibusdam consectetur numquam saepe voluptas quaerat, deleniti quidem aperiam ad quos sint nesciunt molestias nemo iusto necessitatibus, repellat aut dignissimos. Eum',
+    },
+  ],
+  [
+    {
+      current: false,
+      title: 'Online application',
+      subtitle: '02/03/23',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit quibusdam consectetur numquam saepe voluptas quaerat, deleniti quidem aperiam ad quos sint nesciunt molestias nemo iusto necessitatibus, repellat aut dignissimos. Eum',
+    },
+    {
+      current: true,
+      title: 'Phone interview',
+      subtitle: '05/03/23',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit quibusdam consectetur numquam saepe voluptas quaerat, deleniti quidem aperiam ad quos sint nesciunt molestias nemo iusto necessitatibus, repellat aut dignissimos. Eum',
+    },
+    {
+      current: false,
+      title: 'On-site interview',
+      subtitle: '10/03/23',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit quibusdam consectetur numquam saepe voluptas quaerat, deleniti quidem aperiam ad quos sint nesciunt molestias nemo iusto necessitatibus, repellat aut dignissimos. Eum',
+    },
+  ],
+  [
+    {
+      current: false,
+      title: 'Submit application form',
+      subtitle: '15/03/23',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit quibusdam consectetur numquam saepe voluptas quaerat, deleniti quidem aperiam ad quos sint nesciunt molestias nemo iusto necessitatibus, repellat aut dignissimos. Eum',
+    },
+    {
+      current: false,
+      title: 'HR screening',
+      subtitle: '18/03/23',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit quibusdam consectetur numquam saepe voluptas quaerat, deleniti quidem aperiam ad quos sint nesciunt molestias nemo iusto necessitatibus, repellat aut dignissimos. Eum',
+    },
+    {
+      current: true,
+      title: 'Technical assessment',
+      subtitle: '20/03/23',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit quibusdam consectetur numquam saepe voluptas quaerat, deleniti quidem aperiam ad quos sint nesciunt molestias nemo iusto necessitatibus, repellat aut dignissimos. Eum',
+    },
+    {
+      current: false,
+      title: 'Final interview',
+      subtitle: '25/03/23',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit quibusdam consectetur numquam saepe voluptas quaerat, deleniti quidem aperiam ad quos sint nesciunt molestias nemo iusto necessitatibus, repellat aut dignissimos. Eum',
+    },
+  ],
+  [
+    {
+      current: false,
+      title: 'Online application',
+      subtitle: '10/04/23',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit quibusdam consectetur numquam saepe voluptas quaerat, deleniti quidem aperiam ad quos sint nesciunt molestias nemo iusto necessitatibus, repellat aut dignissimos. Eum',
+    },
+    {
+      current: false,
+      title: 'Phone interview',
+      subtitle: '12/04/23',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit quibusdam consectetur numquam saepe voluptas quaerat, deleniti quidem aperiam ad quos sint nesciunt molestias nemo iusto necessitatibus, repellat aut dignissimos. Eum',
+    },
+    {
+      current: true,
+      title: 'In-person interview',
+      subtitle: '15/04/23',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit quibusdam consectetur numquam saepe voluptas quaerat, deleniti quidem aperiam ad quos sint nesciunt molestias nemo iusto necessitatibus, repellat aut dignissimos. Eum',
+    },
+  ],
+]
