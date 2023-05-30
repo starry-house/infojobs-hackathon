92f02a1eb4ae3a2f49e65cf61ba07fad6c439907
SaulMoreyra <armhacam21@gmail.com>
2023-05-29 18:00:19 -0600

    feat(component): implement header bar with close session

---
+++ src/features/hearder/session-header.tsx
@@ -0,0 +1,65 @@
+import {
+  Avatar,
+  Flex,
+  HStack,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Text,
+  useColorModeValue,
+  VStack,
+} from '@chakra-ui/react'
+import { useSession, signOut } from 'next-auth/react'
+
+export const Header = () => {
+  const { data: session } = useSession()
+
+  return (
+    <Flex
+      px={{ base: 4, md: 4 }}
+      height="20"
+      alignItems="center"
+      borderBottomWidth="1px"
+      borderBottomColor={useColorModeValue('gray.200', 'gray.700')}
+      justifyContent={{ base: 'space-between' }}
+    >
+      <Text fontSize="2xl" fontFamily="monospace" fontWeight="bold">
+        Hackaton Infojobs
+      </Text>
+
+      {session && (
+        <HStack spacing={{ base: '0', md: '6' }}>
+          <Flex alignItems={'center'}>
+            <Menu>
+              <MenuButton py={2} transition="all 0.3s">
+                <HStack>
+                  <Avatar size={'sm'} src={session.user.image!} />
+                  <VStack
+                    display={{ base: 'none', md: 'flex' }}
+                    alignItems="flex-start"
+                    spacing="1px"
+                    ml="2"
+                  >
+                    <Text
+                      fontSize="sm"
+                      dangerouslySetInnerHTML={{ __html: session.user.name! }}
+                    />
+                  </VStack>
+                </HStack>
+              </MenuButton>
+              <MenuList
+                bg={useColorModeValue('white', 'gray.900')}
+                borderColor={useColorModeValue('gray.200', 'gray.700')}
+              >
+                <MenuItem onClick={() => signOut({ callbackUrl: '/' })}>
+                  Cerrar sessión
+                </MenuItem>
+              </MenuList>
+            </Menu>
+          </Flex>
+        </HStack>
+      )}
+    </Flex>
+  )
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,11 +3,13 @@ import { ChakraProvider } from '@chakra-ui/react'
 import { SessionProvider } from 'next-auth/react'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { client } from '@/features/shared/client'
+import { Header } from '@/features/hearder'
 
 const App = ({ Component, pageProps: { session, ...pageProps } }: AppProps) => (
   <ChakraProvider>
     <SessionProvider session={session}>
       <QueryClientProvider client={client}>
+        <Header />
         <Component {...pageProps} />
       </QueryClientProvider>
     </SessionProvider>
